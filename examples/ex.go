a66bc144a3911f772cf33ffd768348250d35d665
Frederick Robinson <frederick.robinson@gmail.com>
2018-06-16 23:33:59 -0700

    More examples, a bit of analysis.

--- examples/ex.go
+++ examples/ex.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+func main() {
+	testFn(6)
+	// fmt.Println(testFn(6))
+}
+
+func simplest(x int) int {
+	return x * 2
+}
+
+func testFn(x int) (int, int) {
+	if x < 0 {
+		x *= -1
+	}
+	return x, 1
+}
+
+func fnTwo(x int) int {
+	y := 0
+	for i := 0; i < x; i++ {
+		y += i
+	}
+	return y
+}
--- main.go
+++ main.go
@@ -1,21 +1,82 @@
 package main
 
 import (
-	"golang.org/x/tools/go/loader"
+	"fmt"
 
+	"golang.org/x/tools/go/loader"
 	"golang.org/x/tools/go/ssa"
 	"golang.org/x/tools/go/ssa/ssautil"
 )
 
 func main() {
-	lconf := &loader.Config{}
-	lconf.CreateFromFilenames("./examples", "examples/ex.go")
-
-	lprog, err := lconf.Load()
+	prog, err := createProgram("examples/ex.go")
 	if err != nil {
 		panic(err)
 	}
 
-	mode := ssa.PrintPackages
-	ssautil.CreateProgram(lprog, mode)
+	fn := getFnByName(prog, "simplest")
+	infer(fn)
+}
+
+func createProgram(path string) (*ssa.Program, error) {
+	lconf := &loader.Config{}
+	lconf.CreateFromFilenames("", path)
+
+	lprog, err := lconf.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	prog := ssautil.CreateProgram(lprog, 0)
+	prog.Build()
+	return prog, nil
+}
+
+func getFnByName(prog *ssa.Program, name string) *ssa.Function {
+	fns := ssautil.AllFunctions(prog)
+
+	for fn := range fns {
+		if fn.Name() == name {
+			return fn
+		}
+	}
+	return nil
+}
+
+func infer(fn *ssa.Function) {
+	fmt.Println(fn.Signature)
+	fmt.Println(getRetVals(fn))
+
+	for _, block := range fn.Blocks {
+		fmt.Println("block #", block)
+		fmt.Println("dominees", block.Dominees())
+		for i, instr := range block.Instrs {
+			fmt.Println(i, instr)
+		}
+		fmt.Println("")
+	}
+}
+
+func getRetVals(fn *ssa.Function) ([]ssa.Value, error) {
+	var ans []ssa.Value
+	for _, block := range fn.Blocks {
+		if ret, ok := containsReturn(block); ok {
+			if ans == nil {
+				ans = ret
+			} else {
+				return nil, fmt.Errorf(
+					"can't handle more than one return in fn yet")
+			}
+		}
+	}
+	return ans, nil
+}
+
+func containsReturn(block *ssa.BasicBlock) ([]ssa.Value, bool) {
+	for _, instr := range block.Instrs {
+		if ret, ok := instr.(*ssa.Return); ok {
+			return ret.Results, true
+		}
+	}
+	return nil, false
 }
